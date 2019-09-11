2c44a530baa826f4751c1c66cef6ffdcd962a7da
barbara-chaves <barbarabarcellos90@gmail.com>
2019-09-11 17:46:24 +0200

    create homepage

---
+++ docs/precache-manifest.181477198bf860511c9888e5c12609ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01be91dd95250a5815c8c3d91cbd597",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9468b3a17e070dadf50b",
+    "url": "./static/css/main.7f3fe277.chunk.css"
+  },
+  {
+    "revision": "f06458aafa24eef3acf2",
+    "url": "./static/js/2.8ff387bf.chunk.js"
+  },
+  {
+    "revision": "9468b3a17e070dadf50b",
+    "url": "./static/js/main.984bd969.chunk.js"
+  },
+  {
+    "revision": "4c564400d22926ed1929",
+    "url": "./static/js/runtime~main.003bc98c.js"
+  }
+]);
